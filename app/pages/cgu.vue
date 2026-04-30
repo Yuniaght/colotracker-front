de0f83bce0ca298fc3fd1825a4724fe8433f27ba
Jean-Baptiste de Vulder <devulder.jeanbaptiste@gmail.com>
2026-04-30 12:49:54 +0200

    feat(cgu): add cgu page and directus singleton

--- app/pages/cgu.vue
+++ app/pages/cgu.vue
@@ -1,5 +1,25 @@
+<script setup lang="ts">
+import { readSingleton } from '@directus/sdk';
+const { $directus } = useNuxtApp()
+
+const {data} = await useAsyncData('cgu', () => {
+  return $directus.request(
+    readSingleton('cgu', {
+      fields: [
+        "id",
+        "version",
+        "update_date",
+        "content"
+      ]
+    })
+  )
+})
+</script>
 <template>
-  <div>
-    cgu
-  </div>
+  <section v-html="data?.content" class="responsive-padding-t responsive-padding-x responsive-layout directus-content" />
+  <section class="responsive-layout responsive-padding-x responsive-padding-b pt-4">
+    <p class="text-sm">Mis à jour le : <nuxt-time :datetime="data?.update_date" />. Version : {{ data.version }}. <span @click="console.log('cookies')" class="cursor-pointer select-none">Préférence en matière de cookie</span></p>
+  </section>
 </template>
+<style scoped>
+</style>
