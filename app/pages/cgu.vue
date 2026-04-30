74a41db4f0b44f585288605e4c46ee4c5c185c58
Jean-Baptiste de Vulder <devulder.jeanbaptiste@gmail.com>
2026-04-30 12:52:41 +0200

    feat(cgu): add date formating

--- app/pages/cgu.vue
+++ app/pages/cgu.vue
@@ -18,7 +18,7 @@ const {data} = await useAsyncData('cgu', () => {
 <template>
   <section v-html="data?.content" class="responsive-padding-t responsive-padding-x responsive-layout directus-content" />
   <section class="responsive-layout responsive-padding-x responsive-padding-b pt-4">
-    <p class="text-sm">Mis à jour le : <nuxt-time :datetime="data?.update_date" />. Version : {{ data.version }}. <span @click="console.log('cookies')" class="cursor-pointer select-none">Préférence en matière de cookie</span></p>
+    <p class="text-sm">Mis à jour le : <nuxt-time :datetime="data?.update_date" year="numeric" month="long" day="numeric"/>. Version : {{ data.version }}. <span @click="console.log('cookies')" class="cursor-pointer select-none">Préférence en matière de cookie</span></p>
   </section>
 </template>
 <style scoped>
